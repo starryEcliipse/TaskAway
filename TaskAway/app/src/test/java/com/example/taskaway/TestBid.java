a85a12006f9aa4a2297a56df596b031d2d9cbc6e
aschuldhaus <adrianschuldhaus@gmail.com>
2018-02-21 14:08:38 -0700

    Added TestBid unit test class
    TestBid unit test class contains 3 methods for testing the:
    -constructor
    -getAmount() method
    -setAmount() method

---
+++ TaskAway/app/src/test/java/com/example/taskaway/TestBid.java
@@ -0,0 +1,37 @@
+package com.example.taskaway;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Tests all methods contained in the Bid class
+ */
+
+public class TestBid extends ActivityInstrumentationTestCase2 {
+    public TestBid() {super(Bid.class);}
+
+    /**
+     * Test the constructor
+     */
+    public void testBid(){
+        Bid bid = new Bid(new Float(3.14));
+        assertEquals(bid.getAmount(), 3.14);
+    }
+
+    /**
+     * Test getAmount()
+     */
+    public void testGetAmount(){
+        Bid bid = new Bid(new Float(1.23));
+        assertEquals(bid.getAmount(), 1.23);
+    }
+
+    /**
+     * Test setAmount()
+     */
+    public void testSetAmount(){
+        Bid bid = new Bid();
+        bid.setAmount(new Float(45.67));
+        assertEquals(bid.getAmount(), 45.67);
+    }
+
+}
