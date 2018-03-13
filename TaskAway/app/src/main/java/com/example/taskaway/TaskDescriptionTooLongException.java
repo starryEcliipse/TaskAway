c582da0ed3aa920077f3e8229331b5b876d7c86d
Diane B <boytang@cs.ualberta.ca>
2018-03-13 16:14:26 -0600

    added description to code from earlier

--- TaskAway/app/src/main/java/com/example/taskaway/TaskDescriptionTooLongException.java
+++ TaskAway/app/src/main/java/com/example/taskaway/TaskDescriptionTooLongException.java
@@ -2,6 +2,7 @@ package com.example.taskaway;
 
 /**
  * Created by Diane on 2018-03-13.
+ * Represents the Exception thrown when a task description is greater than 300 chars
  */
 
 public class TaskDescriptionTooLongException extends Exception {
--- TaskAway/app/src/main/java/com/example/taskaway/TaskNameTooLongException.java
+++ TaskAway/app/src/main/java/com/example/taskaway/TaskNameTooLongException.java
@@ -2,6 +2,7 @@ package com.example.taskaway;
 
 /**
  * Created by Diane on 2018-03-13.
+ * Represents the Exception thrown when a task name is greater than 30 chars
  */
 
 public class TaskNameTooLongException extends Exception {
