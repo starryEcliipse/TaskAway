9dd4c479e2f3bff1ceb8972cd6f454aaa83bd454
Diane B <boytang@cs.ualberta.ca>
2018-03-16 19:30:14 -0600

    Layout for viewing details of task from MyBids

--- TaskAway/app/src/main/java/com/example/taskaway/ViewBidTask.java
+++ TaskAway/app/src/main/java/com/example/taskaway/ViewBidTask.java
@@ -1,20 +1,24 @@
 package com.example.taskaway;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
+ * Created by Diane on 2018-03-16.
  * Acts as activity that displays a tasks information when a user selects a task
- * that they HAVE NOT previously bid on.
+ * that they HAVE previously bid on.
  */
-public class ViewTask extends AppCompatActivity {
+
+
+public class ViewBidTask extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_task);
-        // TODO: Buttons - 18/02/18
+        // TODO: Buttons
     }
+
 
     @Override
     protected void onStart(){
