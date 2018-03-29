cb6b5e1f3b8974d883198cc9f2227b42b824939d
Sameerah <wajahat@ualberta.ca>
2018-03-29 16:50:32 -0600

    gallery upload for pic

--- TaskAway/app/src/main/java/com/example/taskaway/UploadPic.java
+++ TaskAway/app/src/main/java/com/example/taskaway/UploadPic.java
@@ -1,13 +1,54 @@
 package com.example.taskaway;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class UploadPic extends AppCompatActivity {
+public class UploadPic extends AppCompatActivity implements View.OnClickListener{
+    private static final int RESULT_LOAD_IMAGE = 1;
+
+    ImageView imageToUpload;
+    Button upload;
+    Button done;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_pic);
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+        upload = (Button) findViewById(R.id.button5);
+        done = (Button) findViewById(R.id.button7);
+
+        done.setOnClickListener(this);
+        upload.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.button7:
+                
+                break;
+
+            case R.id.button5:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+        }
     }
 }
