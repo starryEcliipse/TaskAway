74cc755ccabcd3692c00232c87fccb79908a4269
Sameerah <wajahat@ualberta.ca>
2018-04-03 09:31:08 -0600

    allows multiple selection of images now, work in progress

--- TaskAway/app/src/main/java/com/example/taskaway/UploadPic.java
+++ TaskAway/app/src/main/java/com/example/taskaway/UploadPic.java
@@ -1,17 +1,25 @@
 package com.example.taskaway;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 public class UploadPic extends AppCompatActivity implements View.OnClickListener{
     private static final int RESULT_LOAD_IMAGE = 1;
 
+    LinearLayout linearMain;
     ImageView imageToUpload;
     Button upload;
     Button done;
@@ -21,7 +29,8 @@ public class UploadPic extends AppCompatActivity implements View.OnClickListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_pic);
-        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+        linearMain = (LinearLayout) findViewById(R.id.linearImage);
+        //imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
         upload = (Button) findViewById(R.id.button5);
         done = (Button) findViewById(R.id.button7);
 
@@ -33,12 +42,15 @@ public class UploadPic extends AppCompatActivity implements View.OnClickListener
     public void onClick(View view){
         switch (view.getId()){
             case R.id.button7:
-                
+
                 break;
 
             case R.id.button5:
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(galleryIntent, "android.intent.action.SEND_MULTIPLE"), RESULT_LOAD_IMAGE);
                 break;
         }
     }
@@ -48,7 +60,36 @@ public class UploadPic extends AppCompatActivity implements View.OnClickListener
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
             Uri selectedImage = data.getData();
-            imageToUpload.setImageURI(selectedImage);
+            //imageToUpload.setImageURI(selectedImage);
+            String photoPath = getPath(selectedImage);
+            ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(this).build();
+            ImageLoader.getInstance().init(configuration);
+            //Bitmap bitmap =
         }
+    }
+
+
+    //https://stackoverflow.com/questions/2169649/get-pick-an-image-from-androids-built-in-gallery-app-programmatically
+
+    public String getPath(Uri uri) {
+        // just some safety built in
+        if( uri == null ) {
+            // TODO perform some logging or show user feedback
+            return null;
+        }
+        // try to retrieve the image from the media store first
+        // this will only work for images selected from gallery
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String path = cursor.getString(column_index);
+            cursor.close();
+            return path;
+        }
+        // this is our fallback here
+        return uri.getPath();
     }
 }
