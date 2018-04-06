d3fc02867c576feaca538ab5727b9770704a80e7
Sameerah <wajahat@ualberta.ca>
2018-04-05 23:01:38 -0600

    single image upload + compression

--- TaskAway/app/src/main/java/com/example/taskaway/UploadPic.java
+++ TaskAway/app/src/main/java/com/example/taskaway/UploadPic.java
@@ -12,6 +12,7 @@ import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
@@ -21,6 +22,8 @@ import android.widget.LinearLayout;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import java.lang.reflect.Array;
@@ -29,7 +32,7 @@ import java.util.ArrayList;
 public class UploadPic extends AppCompatActivity implements View.OnClickListener{
     private static final int RESULT_LOAD_IMAGE = 1;
 
-    private Context mContext;
+    //private Context mContext;
     LinearLayout linearMain;
     ArrayList<Uri> arrayU;
     ImageView imageToUpload;
@@ -58,27 +61,49 @@ public class UploadPic extends AppCompatActivity implements View.OnClickListener
     public void onClick(View view){
         switch (view.getId()){
             case R.id.button7:
+                //image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
 
                 break;
 
             case R.id.button5:
                 //https://stackoverflow.com/questions/23426113/how-to-select-multiple-images-from-gallery-in-android
-                Intent galleryIntent = new Intent();
-                galleryIntent.setType("image/*");
-                galleryIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(galleryIntent, "android.intent.action.SEND_MULTIPLE"), RESULT_LOAD_IMAGE);
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                //galleryIntent.setType("image/*");
+                //galleryIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                //galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
                 break;
         }
     }
 
+    //https://stackoverflow.com/questions/13511356/android-image-selected-from-gallery-orientation-is-always-0-exif-tag
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
 
-            Uri selectedImage = data.getClipData().getItemAt(0).getUri();
-            imageToUpload.setImageURI(selectedImage);
+            Uri selectedImage = data.getData();
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            try {
+                BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage), null, options);
+                options.inJustDecodeBounds = false;
+                Bitmap image = BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage), null, options);
+                String type = getContentResolver().getType(selectedImage);
+                Log.i("STUPID TYPE",  type);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                if (type.contains("png")) {
+                    image.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                } else if (type.contains("jpg") || type.contains("jpeg")) {
+                    image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                }
+                imageToUpload.setImageBitmap(image);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            //Uri selectedImage = data.getClipData().getItemAt(0).getUri();
+            //imageToUpload.setImageURI(selectedImage);
 
             //ClipData mClipData = data.getClipData();
             //GridView gridview = (GridView) findViewById(R.id.gridview);
