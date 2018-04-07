17ee8e3509dc6a90bb593676c82b71477484b240
Punam Woosaree <woosaree@ualberta.ca>
2018-04-07 01:04:54 -0600

    EditActivityTests added

--- TaskAway/app/src/androidTest/java/com/example/taskaway/AddTaskActivityTest.java
+++ TaskAway/app/src/androidTest/java/com/example/taskaway/AddTaskActivityTest.java
@@ -65,6 +65,67 @@ public class AddTaskActivityTest extends ActivityInstrumentationTestCase2<AddTas
         solo.clickOnView(solo.getView(R.id.toolbar_save_btn));
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
 
+
+
+
+
+    }
+
+    /**
+     * Tests all the EditTestActivity constraints
+     */
+    public void testEditTask(){
+        AddTaskActivity activity = (AddTaskActivity) solo.getCurrentActivity();
+
+        //Sample Task to edit
+        solo.enterText((EditText) solo.getView(R.id.name_edit_text), "Chores");
+        solo.enterText((EditText) solo.getView(R.id.requirements_edit_text), "Testing is integral to the app");
+        solo.enterText((EditText) solo.getView(R.id.location_edit_text), "University of Alberta");
+        solo.clickOnView(solo.getView(R.id.toolbar_save_btn));
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        //Check if can Edit a task
+        solo.clickInRecyclerView(0);
+        solo.assertCurrentActivity("Wrong Activity", EditActivity.class);
+
+        //Test no input
+        solo.clickOnView(solo.getView(R.id.toolbar_save_btn));
+        solo.assertCurrentActivity("Wrong Activity", EditActivity.class);
+
+        //Test no name input
+        solo.enterText((EditText) solo.getView(R.id.requirements_edit_text), "Just testing purposes");
+        solo.clickOnView(solo.getView(R.id.toolbar_save_btn));
+        solo.assertCurrentActivity("Wrong Activity", EditActivity.class);
+
+        //Test name less than 8 characters
+        solo.enterText((EditText) solo.getView(R.id.name_edit_text), "Chores");
+        solo.clickOnView(solo.getView(R.id.toolbar_save_btn));
+        solo.assertCurrentActivity("Wrong Activity", EditActivity.class);
+
+        //Test no description given
+        solo.clearEditText((EditText) solo.getView(R.id.requirements_edit_text));
+        solo.clickOnView(solo.getView(R.id.toolbar_save_btn));
+        solo.assertCurrentActivity("Wrong Activity", EditActivity.class);
+
+        //Test description greater than 300
+        solo.enterText((EditText) solo.getView(R.id.requirements_edit_text), "So no one told you life was going to be this way. Your job's a joke, you're broke, you're love life's DOA. It's like you're always stuck in second gear, Well, it hasn't been your day, your week, your month, or even your year. But, I'll be there for you, when the rain starts to pour. I'll be there for you, like I've been there before. ");
+        solo.clickOnView(solo.getView(R.id.toolbar_save_btn));
+        solo.assertCurrentActivity("Wrong Activity", EditActivity.class);
+
+        //Correct inputs for edited Task
+        solo.enterText((EditText) solo.getView(R.id.name_edit_text), "Homework");
+        solo.enterText((EditText) solo.getView(R.id.requirements_edit_text), "Testing is integral to the app");
+        solo.enterText((EditText) solo.getView(R.id.location_edit_text), "University of Alberta");
+        solo.clickOnView(solo.getView(R.id.toolbar_save_btn));
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        //Check if can Delete a task
+        solo.clickInRecyclerView(0);
+        solo.assertCurrentActivity("Wrong Activity", EditActivity.class);
+        solo.clickOnView(solo.getView(R.id.DeleteButton));
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+
     }
 
     /**
